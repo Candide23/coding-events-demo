ab991a1f10f1a13759984f308a564ecca73a476a
candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2022-06-14 14:15:05 -0500

    done

--- src/main/java/org/launchcode/codingevents/data/EventRepository.java
+++ src/main/java/org/launchcode/codingevents/data/EventRepository.java
@@ -7,4 +7,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventRepository extends CrudRepository<Event, Integer> {
+
 }
+
+// public class MyRepository implements EventRepository {}
